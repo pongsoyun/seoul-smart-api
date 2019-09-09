bd7f93ba994108c1bbad70dc48d9b5ddf5ebfc14
Eunwoo <dmsdn960@gmail.com>
2019-09-10 00:53:49 +0900

    start express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({success:"실패!!!!"});
+});
+
+app.listen(3000, () => {
+    console.log('listening 3000 port...');
+});
