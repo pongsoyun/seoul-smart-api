009d641732a4164475400ce9ce822da04258c74c
Eunwoo <dmsdn960@gmail.com>
2019-09-10 17:05:56 +0900

    graphql setting with schema & resolver

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const {GraphQLSchema, GraphQLObjectType} = require('graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({success:"실패!!!!"});
 });
 
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const rootValue = {
+    hello: () => 'hello'
+}
+
 app.get('/graphql', graphqlHTTP({
-    schema: new GraphQLSchema({
-        query: new GraphQLObjectType({
-            name: 'RootQueryType',
-        })
-    }),
+    schema,
+    rootValue,
     graphiql:true
-}))
+}));
 
 app.listen(3000, () => {
     console.log('listening 3000 port...');
