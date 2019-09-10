2988b63aec3b52500c9e7738cbfae9e6ba5f671e
Eunwoo <dmsdn960@gmail.com>
2019-09-10 16:40:29 +0900

    install graphql & express-graphql

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const {GraphQLSchema, GraphQLObjectType} = require('graphql');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.json({success:"실패!!!!"});
 });
+
+app.get('/graphql', graphqlHTTP({
+    schema: new GraphQLSchema({
+        query: new GraphQLObjectType({
+            name: 'RootQueryType',
+        })
+    }),
+    graphiql:true
+}))
 
 app.listen(3000, () => {
     console.log('listening 3000 port...');
