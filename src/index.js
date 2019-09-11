afc99b6e647364f3c864f995df5be87691324bf7
Eunwoo <dmsdn960@gmail.com>
2019-09-11 16:15:19 +0900

    create graphql folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { buildSchema } from 'graphql';
 import mongoose from 'mongoose';
-
-const app = express();
+import schema from './graphql/schema';
+import rootValue from './graphql/resolver';
 
 const mongoURI = 'mongodb://seoulapi:seoulapi1@ds127802.mlab.com:27802/heroku_f5g0fz8g';
 
-// 몽고디비 연결
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
   console.log('connnected to mongodb');
@@ -15,21 +13,13 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
   console.error(e);
 });
 
+const app = express();
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ success: '실패!!!!' });
 });
-
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-const rootValue = {
-  hello: () => 'hello',
-};
 
 app.use('/graphql', graphqlHTTP({
   schema,
