896303e87112695a5da92d18d2bfa2c3adbe4388
Eunwoo <dmsdn960@gmail.com>
2019-09-12 16:28:02 +0900

    write schema for graphql

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -4,16 +4,66 @@ export default buildSchema(`
     type Query {
         Users: [User]
         User(token: String): User
-        }
+    }
+
+    type Mutation {
+        createUser(name: String!, token: String!): User
+    }
 
     type User { 
         name: String!
         token: String!
         achievement: Int
         activityLog: [String]
-        }
+    }
+    
+    type Room {
+        name: String!
+        facility: String!
+        visit: Int
+        equipments: [String]
+    }
 
-    type Mutation {
-        createUser(name: String!, token: String!): User
+    type Location {
+        address: String
+        gu: String
+    }
+
+    type Place {
+        name: String!
+        rooms: [Room]
+        location: Location!
+        businessHour: String!
+        bookLink: String
+        thumbnail: String
+        contact: String
+    }
+
+    type Day {
+        date: Date
+        startTime: String
+        progressTime: String
+    }
+
+    type Participant {
+        user: User
+        comment: String
+    }
+
+    type DayInfo {
+        day: Day
+        place: Place
+        room: String
+    }
+
+    type Activity {
+        name: String!
+        leader: User
+        participants: [Participant]
+        total: Int
+        days: [DayInfo]
+        content: String
+        type: String
+        status: String
     }
 `);
