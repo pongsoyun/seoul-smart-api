64c5752042861074bf4cf34ca0b40f23326fd049
Eunwoo <dmsdn960@gmail.com>
2019-09-16 00:59:03 +0900

    setting dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import mongoose from 'mongoose';
@@ -5,6 +6,11 @@ import schema from './graphql/schema';
 import rootValue from './graphql/resolver';
 
 const mongoURI = 'mongodb://seoulapi:seoulapi1@ds127802.mlab.com:27802/heroku_f5g0fz8g';
+
+const {
+  PORT: port = 4000,
+  MONGO_URI: mongoURI
+} = process.env;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useFindAndModify: false }).then(() => {
@@ -23,6 +29,6 @@ app.use('/', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log('listening 4000 port...');
 });
