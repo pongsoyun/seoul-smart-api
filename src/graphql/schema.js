1e46e9fc2821f6a8f5d40a5aa039422cba912896
Eunwoo <dmsdn960@gmail.com>
2019-09-16 21:35:27 +0900

    modify schema what required

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -15,8 +15,8 @@ export default buildSchema(`
     type Mutation {
         createUser(name: String!, token: String!): User
         modifyUser(userId: String!, name: String!): User
-        createActivity(name: String!, userId: String!, total: Int!, date: String, startTime: String, endTime: String, placeId: String!, room: String, content: String, type: String): Activity
-        modifyActivity(activityId: String!, name: String!, userId: String!, total: Int!, date: String, startTime: String, endTime: String, placeId: String, room: String, content: String, type: String): Activity
+        createActivity(name: String!, userId: String!, total: Int!, date: String!, startTime: String!, endTime: String!, placeId: String!, room: String!, content: String!, type: String!): Activity
+        modifyActivity(activityId: String!, name: String!, userId: String!, total: Int!, date: String!, startTime: String!, endTime: String!, placeId: String!, room: String!, content: String!, type: String!): Activity
         deleteActivity(activityId: String!): Activity
         applyActivity(activityId: String!, userId: String!, comment: String!): Activity
         cancelActivity(activityId: String!, userId: String!): Activity
@@ -34,49 +34,54 @@ export default buildSchema(`
     type Room {
         name: String!
         facility: String!
-        visit: Int
-        equipments: [String]
+        equipments: [String]!
         description: String!
         thumbnail: String
     }
 
     type Location {
-        address: String
-        gu: String
+        address: String!
+        gu: String!
     }
 
     type Place {
         name: String!
-        rooms: [Room]
+        rooms: [Room]!
         location: Location!
-        businessHour: String!
+        businessHour: String
         bookLink: String
         thumbnail: String
         contact: String
     }
 
     type Participant {
-        user: User
-        comment: String
+        userId: String!
+        name: String!
+        comment: String!
     }
 
     type Day {
-        date: String
-        startTime: String
-        endTime: String
-        place: Place
-        room: String
+        date: String!
+        startTime: String!
+        endTime: String!
+        place: Place!
+        room: String!
+    }
+
+    type Leader {
+        userId: String!
+        name: String!
     }
 
     type Activity {
         name: String!
-        leader: User
-        participants: [Participant]
-        total: Int
-        days: [Day]
-        content: String
-        type: String
-        status: String
+        leader: Leader!
+        participants: [Participant]!
+        total: Int!
+        days: [Day!]!
+        content: String!
+        type: String!
+        status: String!
     }
 
     type Program {
