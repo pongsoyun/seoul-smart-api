85b923f679ad8d0365ad1b0864d50f111f8bf192
Eunwoo <dmsdn960@gmail.com>
2019-09-24 23:28:20 +0900

    fixed pagenation limits number

--- src/graphql/resolver/user.js
+++ src/graphql/resolver/user.js
@@ -1,28 +1,38 @@
-import User from '../../model/user';
-import { findActivity } from './activity';
+import User from "../../model/user";
+import { findActivity } from "./activity";
 
 export async function Users() {
-    return await User.find();
+  return await User.find();
 }
-  
+
 export async function createUser({ name, token }) {
-  return await User.create({ name, token });
+  const a = await User.create({ name, token });
+  return a;
 }
-  
+
 export async function modifyUser({ userId, name }) {
   return await User.findOneAndUpdate({ _id: userId }, { name });
 }
-  
+
 export async function signIn({ token }) {
   return await User.findOne({ token });
 }
-  
+
 export async function findUser({ _id }) {
   return await User.findOne({ _id });
 }
 
 export async function addLog({ _id, activityId }) {
-  const { name, leader, participants, total, days, content, type, status } = await findActivity({ _id: activityId });
+  const {
+    name,
+    leader,
+    participants,
+    total,
+    days,
+    content,
+    type,
+    status
+  } = await findActivity({ _id: activityId });
   const activity = {
     activityId,
     name,
@@ -31,16 +41,22 @@ export async function addLog({ _id, activityId }) {
     total,
     days,
     content,
-    type, 
-    status,
-  }
-  return await User.findOneAndUpdate({ _id }, { $addToSet: { activityLog: activity }});
+    type,
+    status
+  };
+  return await User.findOneAndUpdate(
+    { _id },
+    { $addToSet: { activityLog: activity } }
+  );
 }
 
 export async function deleteLog({ _id, activityId }) {
-  return await User.findOneAndUpdate({ _id }, { $pull: { activityLog: { activityId }}});
+  return await User.findOneAndUpdate(
+    { _id },
+    { $pull: { activityLog: { activityId } } }
+  );
 }
 
 export async function achieve({ _id, achievement }) {
-  return await User.findOneAndUpdate({ _id }, { $inc : { achievement }});
+  return await User.findOneAndUpdate({ _id }, { $inc: { achievement } });
 }
